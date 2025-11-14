28d816b53aab391dd0d93f190104a29c2df1ba2c
iakingdom <choven0@gmail.com>
2025-11-14 13:05:36 -0700

    First implementation of player drawn on screen

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 def main():
     pygame.init()
     game_clock = pygame.time.Clock()
     dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    player = Player(x,y)
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     while True:
@@ -14,9 +18,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = game_clock.tick(60)
-        print(dt)
 
 if __name__ == "__main__":
     main()
